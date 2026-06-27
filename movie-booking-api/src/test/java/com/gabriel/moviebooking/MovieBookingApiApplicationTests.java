5a85c1ab5c9620b0253940c074fa9f15baaababb
Gabriel <gabrielcoelho7e7@gmail.com>
2026-06-27 16:38:33 -0300

    Initial Spring Boot project setup

---
+++ movie-booking-api/src/test/java/com/gabriel/moviebooking/MovieBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.moviebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
